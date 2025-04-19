5c857784167b412244c2652471041e80830c58dc
Pathum Vimukthi <104794593+Pathum-Vimukthi@users.noreply.github.com>
2025-04-19 22:18:12 +0530

    Implement loading state and add Navbar component with menu functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { LoadingScreen } from "./components/LoadingScreen";
+import { Navbar } from "./components/Navbar";
 import "./index.css";
 
 function App() {
-  return (
-    <>
-      <LoadingScreen />
-    </>
+  const [isLoading, setIsLoading] = useState(true);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const fetchData = () => {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 4500);
+    };
+    fetchData();
+  }, []);
+  return isLoading ? (
+    <LoadingScreen />
+  ) : (
+    <div
+      className={`min-h-screen transition-opacity duration-700 ${
+        !isLoading ? "opacity-100" : "opacity-0"
+      } bg-black text-gray-100`}
+    >
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+    </div>
   );
 }
 
